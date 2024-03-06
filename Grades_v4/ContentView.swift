012babcd5b899cdb5dba067089e4c9e506447215
Jakob <jakob.erlebach@icloud.com>
2024-03-06 19:36:49 +0100

    Update Notenkasten.swift

--- Grades_v4/ContentView.swift
+++ Grades_v4/ContentView.swift
@@ -10,42 +10,55 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var storage: storageclass
     var body: some View {
-        NavigationView{
-            ScrollView{
-                Divider()
-                ForEach(storage.fächer, id: \.self) { fach in
-                    Button(action: {
-                        storage.activeview = .fach
-                    }, label: {
-                        Fachkachel(color: fach.color, fach: fach.name)
-                    })
-                    
-                
-            }
-                
-            }.navigationBarItems(trailing: Button(action: {
-                
-            }){
-                Image(systemName: "plus.rectangle.on.folder.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .padding(4)
-                    .frame(width: 45, height: 35)
-
-                    .foregroundColor(.blue)
-            } )
-            .navigationTitle("Grades").frame(maxWidth: .infinity, alignment: .leading)
+        ZStack{
             
+            Section{
+                NavigationView{
+                    ScrollView{
+                        Divider()
+                        ForEach(storage.fächer, id: \.self) { fach in
+                            Button(action: {
+                                storage.activeview = .fach
+                            }, label: {
+                                Fachkachel(fachspeicher: fach)
+                            })
+                            
+                            
+                        }
+                        
+                    }.navigationBarItems(trailing: Button(action: {
+                        storage.addfach = true
+                    }){
+                        Image(systemName: "plus.rectangle.on.folder.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(4)
+                            .frame(width: 45, height: 35)
+                        
+                            .foregroundColor(.blue)
+                    } )
+                    .navigationTitle("Grades").frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
+            if storage.addfach{
+                VisualEffectView(effect: UIBlurEffect(style: .dark))
+                    .ignoresSafeArea(edges: .all)
+                    
+                addfileview(storage: storage)
+                    .padding(.horizontal, UIScreen.main.bounds.width / 6)
+
+            }
         }
     }
 }
-/*
-struct ContentView_Previews: PreviewProvider {
-    
-    @StateObject var storage: storageclass = storageclass()
-    
-    static var previews: some View {
-        ContentView(storage: storage)
-    }
+
+
+struct VisualEffectView: UIViewRepresentable {
+    var effect: UIVisualEffect?
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
 }
-*/
+
+#Preview {
+    Wrapper()
+}
